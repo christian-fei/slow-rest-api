537b54b03bf903247214ddb731a36327638b3eba
Christian Fei <crifei93@gmail.com>
2018-05-10 12:39:24 +0200

    handle etag deopt and improv error handling

--- etag.js
+++ etag.js
@@ -2,8 +2,6 @@
 
 var crypto = require('crypto')
 
-var etagCache = require('./etag-cache.json')
-
 module.exports = etag
 
 function etag (entity, opts) {
@@ -12,23 +10,10 @@ function etag (entity, opts) {
     entity = opts.entity
   }
 
-  var error = false
   opts = opts || {}
   opts.algorithm = opts.algorithm || 'md5'
   opts.encoding = opts.encoding || 'utf8'
   opts.output = opts.output || 'base64'
-
-  var match
-
-  Object.keys(etagCache).forEach(function (k) {
-    match = (etagCache[k].algorithm === opts.algorithm &&
-      etagCache[k].encoding === opts.encoding &&
-      etagCache[k].output === opts.output &&
-      entity === Buffer.from(etagCache[k].content.data).toString()) &&
-      k
-  })
-
-  if (match) { return match }
 
   var hash
 
@@ -37,7 +22,7 @@ function etag (entity, opts) {
       .createHash(opts.algorithm)
       .update(entity, opts.encoding)
   } catch (e) {
-    error = true
+    return Error('oh oh')
   }
 
   if (!opts.output || opts.output === 'base64') {
@@ -46,21 +31,15 @@ function etag (entity, opts) {
         .digest('base64')
         .replace(/=+$/, '')
     } catch (e) {
-      error = true
+      return Error('oh oh')
     }
 
-    if (!error) {
-      return hash
-    }
+    return hash
   }
 
   try {
     hash = hash.digest(opts.output)
   } catch (e) {
-    error = true
-  }
-
-  if (error) {
     return Error('oh oh')
   }
 
